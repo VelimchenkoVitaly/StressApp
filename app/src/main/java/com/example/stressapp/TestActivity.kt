2b956a7adcd1fdc703d0df3b0ae5c711d0adc4e6
VelimchenkoVitaly <132755600+VelimchenkoVitaly@users.noreply.github.com>
2023-07-31 10:15:21 +0300

    исправил подсчет очков тестирования

--- app/src/main/java/com/example/stressapp/TestActivity.kt
+++ app/src/main/java/com/example/stressapp/TestActivity.kt
@@ -1,124 +1,130 @@
 package com.example.stressapp
 
-import android.content.res.Resources
-import android.graphics.Color
-import android.icu.text.AlphabeticIndex.ImmutableIndex
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.provider.MediaStore.Audio.Radio
-import android.view.ViewGroup
-import android.view.ViewGroup.MarginLayoutParams
-import android.widget.LinearLayout
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.RelativeLayout
-import androidx.core.view.marginTop
-import android.view.Gravity
-import android.widget.TextView
-import android.widget.Button
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 
 class TestActivity : AppCompatActivity() {
+    private var wellbeing : Int = 0
+    private var activeness : Int = 0
+    private var mood : Int = 0
+    private var wellbeingTemp : Int = 0
+    private var activenessTemp : Int = 0
+    private var moodTemp : Int = 0
+    private var buttonFlag : Int = 0
+    private val negativeListL: List<String> = listOf("Самочувствие плохое", "Чувствую себя слабым", "Разбитый", "Обессиленный", "Напряженный", "Больной", "Усталый", "Изнуренный", "Легко утомляемый", "Вялый", "Пассивный", "Малоподвижный", "Медлительный", "Бездеятельный", "Безучастный", "Равнодушный", "Сонливый", "Желание отдохнуть", "Соображать трудно", "Рассеянный", "Грустный", "Плохое настроение", "Несчастный", "Мрачный", "Унылый", "Печальный", "Озабоченный", "Пессимистичный", "Разочарованный", "Недовольный", "")
+    private val positiveListR: List<String> = listOf("Самочувствие хорошее", "Чувствую себя сильным", "Работоспособный", "Полный сил", "Расслабленный", "Здоровый", "Отдохнувший", "Свежий", "Выносливый", "Бодрый", "Активный", "Подвижный", "Быстрый", "Деятельный", "Увлеченный", "Взволнованный", "Возбужденный", "Желание работать", "Соображать легко", "Внимательный", "Веселый", "Хорошее настроение", "Счастливый", "Жизнерадостный", "Восторженный", "Радостный", "Спокойный", "Оптимистичный", "Полный надежд", "Довольный", "")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
-        setTest()
 
+        val rGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        val nextBtn = findViewById<Button>(R.id.button3)
+        val txtL = findViewById<TextView>(R.id.textL)
+        val txtR = findViewById<TextView>(R.id.textR)
+        //val txt1 = findViewById<TextView>(R.id.textView1)
+        //val txt2 = findViewById<TextView>(R.id.textView2)
+        //val txt3 = findViewById<TextView>(R.id.textView3)
 
+        //val txt4 = findViewById<TextView>(R.id.textView5)
+        //txt4.text = buttonFlag.toString()
+        txtL.text = ""
+        txtR.text = ""
+        nextBtn.text = "начать"
+        rGroup.visibility = View.INVISIBLE
+        nextBtn.setOnClickListener{
+            rGroup.visibility = View.VISIBLE
+                txtL.text = negativeListL[buttonFlag]
+                txtR.text = positiveListR[buttonFlag]
+                nextBtn.text = "следующее состояние"
+
+                when (buttonFlag) {
+                    in 0..10 -> {
+                        wellbeing += wellbeingTemp
+                    }
+                    in 11..20 -> {
+                        activeness += activenessTemp
+                    }
+                    in 21..30 -> {
+                        mood += moodTemp
+                    }
+                }
+               // txt1.setText(wellbeing.toString())
+                //txt2.setText(activeness.toString())
+                //txt3.setText(mood.toString())
+               // txt4.text = buttonFlag.toString()
+            if (buttonFlag < 30)
+                buttonFlag++
+            else {
+                nextBtn.text = "закончить"
+                createExitDialog()
+            }
+                rGroup.clearCheck()
+        }
+
+        rGroup.setOnCheckedChangeListener{group, checkedId ->
+            when(checkedId)
+            {
+                R.id.radioButton1 -> {
+                    when (buttonFlag){
+                        in 0 ..10 -> {wellbeingTemp = 1}
+                        in 11..20 -> {activenessTemp = 1}
+                        in 21..30 -> {moodTemp = 1}}
+                }
+                R.id.radioButton2 -> {
+                    when (buttonFlag){
+                        in 0 ..10 -> {wellbeingTemp = 2}
+                        in 11..20 -> {activenessTemp = 2}
+                        in 21..30 -> {moodTemp = 2}}
+                }
+                R.id.radioButton3 -> {
+                    when (buttonFlag){
+                        in 0 ..10 -> {wellbeingTemp = 3}
+                        in 11..20 -> {activenessTemp = 3}
+                        in 21..30 -> {moodTemp = 3}}
+                }
+                R.id.radioButton4 -> {
+                    when (buttonFlag){
+                        in 0 ..10 -> {wellbeingTemp = 4}
+                        in 11..20 -> {activenessTemp = 4}
+                        in 21..30 -> {moodTemp = 4}}
+                }
+                R.id.radioButton5 -> {
+                    when (buttonFlag){
+                        in 0 ..10 -> {wellbeingTemp = 5}
+                        in 11..20 -> {activenessTemp = 5}
+                        in 21..30 -> {moodTemp = 5}}
+                }
+                R.id.radioButton6 -> {
+                    when (buttonFlag){
+                        in 0 ..10 -> {wellbeingTemp = 6}
+                        in 11..20 -> {activenessTemp = 6}
+                        in 21..30 -> {moodTemp = 6}}
+                }
+                R.id.radioButton7 -> {
+                    when (buttonFlag){
+                        in 0 ..10 -> {wellbeingTemp = 7}
+                        in 11..20 -> {activenessTemp = 7}
+                        in 21..30 -> {moodTemp = 7}}
+                }
+            }
+
+        }
     }
 
-    private fun setTest()
-{
-    var buttonFlag : Int = 1
+    private fun createExitDialog()
+    {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(R.string.testPassed)
+        builder.setMessage((wellbeing/10).toString() + "\n" + (activeness/10).toString()+ "\n" + (mood/10).toString())
 
-    val listR: List<String> = listOf("Самочувствие плохое","Чувствую себя слабым", "Активный", "Подвижный", "Грустный", "Плохое настроение", "Разбитый", "Обессиленный", "Быстрый", "Деятельный", "Несчастный", "Мрачный", "Расслабленный", "Больной", "Увлеченный", "Взволнованный", "Унылый", "Печальный", "Усталый", "Изнуренный", "Возбужденный", "Желание работать", "Озабоченный", "Пессимистичный", "Легко утомляемый", "Вялый", "Соображать легко", "Внимательный", "Разочарованный", "Недовольный")
-    val listL: List<String> = listOf("Самочувствие хорошее", "Чувствую себя сильным", "Пассивный", "Малоподвижный", "Веселый", "Хорошее настроение", "Работоспособный", "Полный сил", "Медлительный", "Бездеятельный", "Счастливый", "Жизнерадостный", "Напряженный", "Здоровый", "Безучастный", "Равнодушный", "Восторженный", "Радостный", "Отдохнувший", "Свежий", "Сонливый", "Желание отдохнуть", "Спокойный", "Оптимистичный", "Выносливый", "Бодрый", "Соображать трудно", "Рассеянный", "Полный надежд", "Довольный")
-    val layout = findViewById<RelativeLayout>(R.id.layout)
-
-    val paramsRG = RelativeLayout.LayoutParams(
-        RelativeLayout.LayoutParams.MATCH_PARENT,
-        RelativeLayout.LayoutParams.WRAP_CONTENT
-    )
-    paramsRG.addRule(RelativeLayout.CENTER_VERTICAL)
-
-
-    val rGroup = RadioGroup(this)
-    rGroup.gravity = Gravity.CENTER_HORIZONTAL
-    rGroup.orientation = LinearLayout.HORIZONTAL
-    rGroup.layoutParams = paramsRG
-
-    val paramsTxtR = RelativeLayout.LayoutParams(
-        RelativeLayout.LayoutParams.MATCH_PARENT,
-        RelativeLayout.LayoutParams.WRAP_CONTENT
-    )
-
-    paramsTxtR.setMargins(0,1200,10,0)
-
-    val paramsTxtL = RelativeLayout.LayoutParams(
-        RelativeLayout.LayoutParams.MATCH_PARENT,
-        RelativeLayout.LayoutParams.WRAP_CONTENT
-    )
-
-    paramsTxtL.setMargins(10,1200,0,0)
-    val txtR = TextView(this)
-
-    txtR.layoutParams = paramsTxtR
-    txtR.gravity = Gravity.RIGHT
-    txtR.setTextColor(Color.BLACK)
-    txtR.textSize = 17F
-
-    val txtL = TextView(this)
-    txtR.text = listR[0]
-    txtL.text = listL[0]
-    txtL.layoutParams = paramsTxtL
-    txtL.gravity = Gravity.LEFT
-    txtL.setTextColor(Color.BLACK)
-    txtL.textSize = 17F
-    //var x = 0
-    val rBut1 = RadioButton(this)
-    rBut1.text = "3"
-    val rBut2 = RadioButton(this)
-    rBut2.text = "2"
-    val rBut3 = RadioButton(this)
-    rBut3.text = "1"
-    val rBut4 = RadioButton(this)
-    rBut4.text = "0"
-    val rBut5 = RadioButton(this)
-    rBut5.text = "1"
-    val rBut6 = RadioButton(this)
-    rBut6.text = "2"
-    val rBut7 = RadioButton(this)
-    rBut7.text = "3"
-
-    val paramsBtn = RelativeLayout.LayoutParams(
-        RelativeLayout.LayoutParams.MATCH_PARENT,
-        RelativeLayout.LayoutParams.WRAP_CONTENT
-    )
-    paramsBtn.setMargins(0,1300,0,0)
-
-    val OKbtn = Button(this)
-    OKbtn.text = "следующее состояние"
-    OKbtn.layoutParams = paramsBtn
-    OKbtn.gravity = Gravity.CENTER_HORIZONTAL
-    OKbtn.setBackgroundColor(Color.RED)
-
-    OKbtn.setOnClickListener{
-        if (buttonFlag <= 30){
-        txtR.text = listR[buttonFlag]
-        txtL.text = listL[buttonFlag]
-        buttonFlag++}
-        //else сюда дописать вывод результатов после их подсчета
+        builder.setNegativeButton(R.string.exit) { dialogInterface, i -> startActivity(Intent(this, MainActivity::class.java )) }
+        builder.show()
     }
-    rGroup.addView(rBut1)
-    rGroup.addView(rBut2)
-    rGroup.addView(rBut3)
-    rGroup.addView(rBut4)
-    rGroup.addView(rBut5)
-    rGroup.addView(rBut6)
-    rGroup.addView(rBut7)
-    layout.addView(rGroup)
-    layout.addView(OKbtn)
-    layout.addView(txtR)
-    layout.addView(txtL)
-}
+
 }
